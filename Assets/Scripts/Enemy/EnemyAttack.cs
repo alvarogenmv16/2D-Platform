f53ba25911026ac68cf95925af5b770664f7f066
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-08-13 20:20:32 +0200

    feat: Basic script for enemy attack

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    // =========================
+    // VARIABLES
+    // =========================
+
+    // =========================
+    // START
+    // =========================
+
+    private void Start()
+    {
+    }
+
+    // =========================
+    // UPDATE / FIXED UPDATE
+    // =========================
+
+    // =========================
+    // FUNCTIONS
+    // =========================
+
+    public void Attack()
+    {
+        Debug.Log("Enemy attacks!");
+    }
+}
