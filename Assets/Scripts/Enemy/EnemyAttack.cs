6de6cf331cb6fc6b3fdd9a7dbc6364eab0f409c7
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-08-13 20:34:49 +0200

    feat: Cooldown between enemy attacks

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,7 +5,9 @@ public class EnemyAttack : MonoBehaviour
     // =========================
     // VARIABLES
     // =========================
+    [SerializeField] private float attackCooldown = 1f;
 
+    private float attackCooldownTimer = 0f;
     // =========================
     // START
     // =========================
@@ -17,13 +19,26 @@ public class EnemyAttack : MonoBehaviour
     // =========================
     // UPDATE / FIXED UPDATE
     // =========================
-
+    private void FixedUpdate()
+    {
+        if (attackCooldownTimer > 0f)
+        {
+            attackCooldownTimer -= Time.fixedDeltaTime;
+        }
+    }
     // =========================
     // FUNCTIONS
     // =========================
 
     public void Attack()
     {
+        if (attackCooldownTimer > 0f)
+        {
+            return;
+        }
+
         Debug.Log("Enemy attacks!");
+
+        attackCooldownTimer = attackCooldown;
     }
 }
